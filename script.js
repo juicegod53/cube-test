09e243650c2d70bb78dd347742179fdfc04d7ea4
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-03-16 18:10:44 +0000

    Added timer functionality and time tracking

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let text_box = document.getElementById("timer-alg")
-let timer_text = document.getElementById("timer-text");
+let active = false;
+let timer_text = document.getElementById("timer-text")
+let alg_text = document.getElementById("timer-alg")
+let sessionTimes = [];
 
+let times = document.getElementById("times")
 
 function generate_alg() {
     const moves = ["L", "R", "B", "F", "D", "U"];
@@ -9,6 +12,7 @@ function generate_alg() {
     let last_move_checks = [["L","R"], ["B", "F"], ["D", "U"]];
     let move_double;
     let last_two_moves = [];
+
     for (let x = 0; x < 19; x++) {
         let move = moves[Math.floor(Math.random()*moves.length)];
         if (algorithm.length >= 2) {
@@ -30,19 +34,42 @@ function generate_alg() {
 
         algorithm.push(move)
     }
-    text_box.innerText = algorithm.join(" ")
+    alg_text.innerText = algorithm.join(" ")
+}
+
+function calculateTime(event) {
+    if (event.key == " ") {
+        if (active == false) {
+            startTime = new Date
+            active = true
+            timerInterval = setInterval(updateTimer, 100)
+        } else {
+            clearInterval(timerInterval)
+            finishTime = new Date
+            timeTaken = Math.round(((finishTime - startTime) * 0.001) * 100) / 100
+            timeTakenString = timeTaken.toFixed(2)
+            active = false
+            updateSessionTimes(timeTaken, alg_text.innerText)
+            timer_text.innerText = timeTakenString
+            generate_alg()
+        }
+    }
+}
+
+function updateTimer() {
+    currentTime = new Date
+    timeShown = (Math.round(((currentTime - startTime) * 0.001) * 10) / 10).toFixed(1)
+    timer_text.innerText = timeShown
+}
+
+function updateSessionTimes(time, algorithm) {
+    sessionTimes.push([time, algorithm])
+    const timeItem = document.createElement("li")
+    timeItem.innerText = sessionTimes[sessionTimes.length - 1][0] + ": " + sessionTimes[sessionTimes.length - 1][1]
+    times.appendChild(timeItem)
 }
 
 generate_alg()
 
-window.onkeydown = function(e) {
-    if (e.key == " ") {
-        timer_text.style.color = "red";
-    }
-}
+window.addEventListener("keypress", calculateTime)
 
-window.onkeyup = function(e) {
-    if (e.key == " ") {
-        timer_text.style.color = "white";
-    }
-}
